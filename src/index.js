2b66b4d605486727775013fea58aab0672e18cb8
saikrish18 <shivarathrisaikrish96@gmail.com>
2023-10-18 19:29:25 +0530

    Final commits

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+
 import { AppRegistry } from 'react-native';
-import App from './App'; 
-import appJson from './app.json';
+import App from './App';
+import { name as appName } from './src/app.json';
+import { initializeApp } from '@react-native-firebase/app';
 
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
 
+initializeApp(firebaseConfig);
 
 AppRegistry.registerComponent(appName, () => App);
 AppRegistry.runApplication(appName, { rootTag: document.getElementById('app-root') });
